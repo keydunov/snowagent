14ff72b28ebd007c0bc531a5d612ff545beea873
Alexey Vakhov <vakhov@gmail.com>
2015-02-06 23:28:38 +0300

    Clean railtie a little

--- lib/snowagent/railtie.rb
+++ lib/snowagent/railtie.rb
@@ -5,18 +5,10 @@ module SnowAgent
     initializer "snowagent.subscribe_notifications" do
       ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*args| 
         event = ActiveSupport::Notifications::Event.new(*args)
-        controller = event.payload[:controller]
-        action = event.payload[:action]
         duration = event.duration/1000.0
 
         # Send only 'request' metric for now
-
-        #SnowAgent.metric("#{controller}##{action}.total_time", duration, :specific_request)
         SnowAgent.metric("request", duration, :request)
-
-        # TODO: don't send these metrics so frequent and so ugly
-        #SnowAgent.metric("controlles", ApplicationController.subclasses.size, :controllers)
-        #SnowAgent.metric("models", ActiveRecord::Base.subclasses.size, :models)
       end
     end
   end
