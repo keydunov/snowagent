e1801c778dc93c4e03067e137fd53facd8c84510
Alexey Vakhov <vakhov@gmail.com>
2015-02-22 22:57:04 +0300

    Count only real GET-requests

--- lib/snowagent/railtie.rb
+++ lib/snowagent/railtie.rb
@@ -4,10 +4,12 @@ module SnowAgent
     initializer "snowagent.subscribe_notifications" do
       ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*args| 
         event = ActiveSupport::Notifications::Event.new(*args)
-        duration = event.duration/1000.0
 
-        # Send only 'request' metric for now
-        SnowAgent.metric("request", duration, :request)
+        # Count only `html` GET requets
+        if event.payload[:method] == "GET" && event.payload[:format] == :html
+          duration = event.duration/1000.0
+          SnowAgent.metric("request", duration, :request)
+        end
       end
     end
   end
